c3cf1ef5ccf609d1a0976d9aabc5ddc2b50fda74
Caswell Orr <caswell.orr@gmail.com>
2021-12-20 18:51:56 -0600

    tag route finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,11 +5,9 @@ const { Category, Product, ProductTag } = require('../../models');
 
 // find all categories
 router.get('/', (req, res) => {
-
   try {
-
     const categoryData = await Category.findAll();
-    // be sure to include its associated Products
+    // be sure to include its associated Products??
     res.status(200).json(categoryData);
 
   } catch (err) {
@@ -19,7 +17,6 @@ router.get('/', (req, res) => {
 
 // find one category by its `id` value
 router.get('/:id', (req, res) => {
-
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       // JOIN with products, using the productTag through table
@@ -30,7 +27,7 @@ router.get('/:id', (req, res) => {
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
       return;
-    }
+    };
 
     res.status(200).json(categoryData);
 
@@ -41,7 +38,6 @@ router.get('/:id', (req, res) => {
 
 // create a new category
 router.post('/', (req, res) => {
-
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -53,21 +49,18 @@ router.post('/', (req, res) => {
 
 // update a category by its `id` value
 router.put('/:id', (req, res) => {
-
   try {
     const categoryData = await Category.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    
+
     res.status(200).json(categoryData);
 
   } catch (err) {
     res.status(400).json(err);
   }
-
-
 });
 
 // delete a category by its `id` value
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,34 +5,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-
   try {
-
     const categoryData = await Category.findAll();
-   
-    // be sure to include its associated Category and Tag data
+    // be sure to include its associated Category and Tag data??
     res.status(200).json(categoryData);
     
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 // get a single product by its `id`
 router.get('/:id', (req, res) => {
-  
   try {
     const productData = await Product.findByPk(req.params.id, {
       // JOIN with category, using the productTag through table
-      // be sure to include its associated product and Tag data
+      // be sure to include its associated Category and Tag data
       include: [{ model: Category, through: ProductTag, as: 'product_categories' }]
     });
 
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
       return;
-    }
+    };
 
     res.status(200).json(categoryData);
 
@@ -127,7 +122,7 @@ router.delete('/:id', (req, res) => {
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
       return;
-    }
+    };
 
     res.status(200).json(productData);
 
